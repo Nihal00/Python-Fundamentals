864ebde1f4c439715c0df4e428ea268c296b5dc6
Nihal00 <nihallsuvarna@gmail.com>
2025-12-03 23:02:08 +0530

    Enhance guess the number game with customizable range and attempt limit

--- guess_number.py
+++ guess_number.py
@@ -3,8 +3,11 @@ import random
 def guess_number_game():
     print("Welcome to the guess the number game")
     print("I have selected the number between 1 to 100 guess the number")
+    min_value = int(input("Enter the minimum value: "))   
+    max_value = int(input("Enter the maximum value: "))
+    num_attempts = int(input("Enter the number of attempts you want to have: "))
 
-    select_random_num = random.randint(1, 100)
+    select_random_num = random.randint(min_value, max_value)
     attempt = 0
 
     # Go through while loop
@@ -13,7 +16,10 @@ def guess_number_game():
             user_guess = int(input("Write the number you guessed: "))
             attempt += 1
 
-            if select_random_num < user_guess:
+            if attempt > num_attempts:
+                print("You have reached the limit")
+                break
+            elif select_random_num < user_guess:
                 print("You are higher to the guess number")
             elif select_random_num > user_guess:
                 print("You are lower to the guess number")
@@ -23,8 +29,6 @@ def guess_number_game():
                 break
         except ValueError:
             print("You can only select number")
-
-
 
 
 if __name__ == "__main__":
